2a8ff2aa7ef43b5e2b523c601897f5320a1a2ec0
omar ali <omarali0601520286@gmail.com>
2020-09-24 11:43:48 +0100

    file upload and form submition

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,7 +10,8 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
-
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Spatie\SimpleExcel\SimpleExcelReader;
 
 class RegistrationController extends AbstractController
 {
@@ -32,10 +33,12 @@ class RegistrationController extends AbstractController
     public function registerNewMember(Request $request)
     {
         // validation
+        // TODO :: ad validation 
+        // rasemblade des data
         $data = json_decode($request->getContent(), true);
 
         // age du member
-        $age = DateTime::createFromFormat('Y-m-d',$data["date_de_naissance"] )->diff(new DateTime('now'))->y;
+        $age = DateTime::createFromFormat('d/m/Y',$data["date_de_naissance"] )->diff(new DateTime('now'))->y;
         // get totqle saved lins to db and test if over 11
         $countMmebersInDB =$this->getDoctrine()->getManager()
         ->getRepository(Member::class)
@@ -53,18 +56,18 @@ class RegistrationController extends AbstractController
                 ->select('AVG(DATE_DIFF(CURRENT_DATE(),a.date_de_naissance)/356) AS age')
                 ->getQuery()
                 ->getSingleScalarResult();
-            if(($avgOfAges * 0.9) > $age && $age > ($avgOfAges * 1.1))
-                return $this->json(["error"=>"age : " . $age . "nest pas dans les limit des 10% " . ($avgOfAges * 0.9) . " - " . ($avgOfAges * 1.1) ],400);
+            if(($avgOfAges * 0.9) > $age || $age > ($avgOfAges * 1.1))
+                return $this->json(["error"=>"Age : " . $age . " n'est pas dans les limites des 10% " . floor($avgOfAges * 0.9) . " - " . floor($avgOfAges * 1.1) ],400);
         }else{
             if ($data["pays"] == "M") {
                 // si marocain l'age doit etre 16 ou plus
                 if($age < 16)
-                    return $this->json(["error"=>"age non acceptable"],400);
+                    return $this->json(["error"=>"Age non acceptable 16 ou pus"],400);
 
             } else if ($data["pays"] == "E") {
                 // si etranger l'age doit etre 18 ou plus
                 if($age < 18)
-                    return $this->json(["error"=>"age non acceptable"],400);
+                    return $this->json(["error"=>"Age non acceptable 18 ou plus"],400);
             }            
         }
 
@@ -80,8 +83,8 @@ class RegistrationController extends AbstractController
         $member = new Member();
         $member->setNom($data["nom"]);
         $member->setPrenom($data["prenom"]);
-        $member->setDateInscription(DateTime::createFromFormat('Y-m-d',date('Y-m-d') ));
-        $member->setDateDeNaissance(DateTime::createFromFormat('Y-m-d',$data["date_de_naissance"] ));
+        $member->setDateInscription(DateTime::createFromFormat('d/m/Y',date('d/m/Y') ));
+        $member->setDateDeNaissance(DateTime::createFromFormat('d/m/Y',$data["date_de_naissance"] ));
         $member->setPays($data["pays"]);
         $member->setEtat($etat);
         $member->setSexe($data["sexe"]);
@@ -97,7 +100,53 @@ class RegistrationController extends AbstractController
     }
 
 
+    public function registerNewMembersByFile(Request $request, SluggerInterface $slugger)
+    {
+        $originalFilename = pathinfo($request->files->get('membersfile')->getClientOriginalName(), PATHINFO_FILENAME);
+        // this is needed to safely include the file name as part of the URL
+        $safeFilename = $slugger->slug($originalFilename);
+        $newFilename = $safeFilename.'.'.$request->files->get('membersfile')->guessExtension();
 
+        $uploadedFile = $request->files->get('membersfile');
+        $destination = $this->getParameter('kernel.project_dir').'/public/uploads';
+        $uploadedfilePath = $uploadedFile->move($destination,$newFilename)->getrealPath();
+        $idsOfMembers = [];
+        SimpleExcelReader::create($uploadedfilePath)
+        ->getRows()
+        ->each(function(array $rowProperties) use (&$idsOfMembers) {
+            // save to database from file
+            // si time entre 12 - 21 etat = V (valide) si non etat = NV (non valide)
+            $etat = ($this->TimeIsBetweenTwoTimes(date("H:i:s"))) ? "V" : "NV" ;
+
+
+            // you can fetch the EntityManager via $this->getDoctrine()
+            // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+            $entityManager = $this->getDoctrine()->getManager();
+            // data integration to model
+            $member = new Member();
+            $member->setNom($rowProperties["nom"]);
+            $member->setPrenom($rowProperties["prenom"]);
+            $member->setDateInscription(DateTime::createFromFormat('d/m/Y',date('d/m/Y') ));
+            $member->setDateDeNaissance(DateTime::createFromFormat('d/m/Y',$rowProperties["date_de_naissance"] ));
+            $member->setPays($rowProperties["pays"]);
+            $member->setEtat($etat);
+            $member->setSexe($rowProperties["sexe"]);
+            $member->setTelephone($rowProperties["telephone"]);
+
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($member);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+            array_push($idsOfMembers,$member->getId());
+            
+        });
+        
+        return $this->json($idsOfMembers,200);
+    }
+
+
+    // helper function
     function TimeIsBetweenTwoTimes($input) {
         $from = "12:00:00";
         $till = "21:00:00";
@@ -107,4 +156,8 @@ class RegistrationController extends AbstractController
         if ($f > $t) $t->modify('+1 day');
         return ($f <= $i && $i <= $t) || ($f <= $i->modify('+1 day') && $i <= $t);
     }
+
+
+
+
 }
